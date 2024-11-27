3bd26d951930a323fe9ab129bb947dd26704f34b
Bastian-Bouffanais <bastibouffanais@gmail.com>
2024-11-27 16:28:24 -0300

    corregidos errores

--- gipit-back/app/api/candidate_process/route.ts
+++ gipit-back/app/api/candidate_process/route.ts
@@ -3,9 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-
 export async function POST(req: NextRequest) {
-  const { candidate_id, process_id, technical_skills, soft_skills, client_comments, match_percent, interview_questions} = await req.json();
+  const { candidate_id, process_id, technical_skills, soft_skills, client_comments, match_percent, interview_questions } = await req.json();
 
   try {
     const candidateProcess = await prisma.candidate_process.create({
@@ -22,10 +21,9 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(candidateProcess, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: `Error al asociar candidato al proceso: ${error.message}` }, { status: 500 });
+    return NextResponse.json({ error: `Error al postear candidate_process: ${error}` }, { status: 500 });
   }
 }
-
 
 export async function GET() {
   try {
@@ -34,11 +32,11 @@ export async function GET() {
     });
 
     if (!candidateProcesses || candidateProcesses.length === 0) {
-      return NextResponse.json({ error: 'No candidate-process relationships found' }, { status: 404 });
+      return NextResponse.json({ error: 'No se encontraron relaciones de candidato-proceso' }, { status: 404 });
     }
 
     return NextResponse.json(candidateProcesses);
   } catch (error) {
-    return NextResponse.json({ error: `Error fetching data: ${error.message}` }, { status: 500 });
+    return NextResponse.json({ error: `Error al actualizar la gestión de usuarios: ${error}` }, { status: 500 });
   }
 }
