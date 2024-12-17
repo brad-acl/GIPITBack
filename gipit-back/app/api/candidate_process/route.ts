22e153423381c71a9b7b59c0f22a84f1b866d400
santiagoneiraACL <sneira@acl.cl>
2024-12-17 20:40:09 -0300

    Se agrego campo de total_experience

--- gipit-back/app/api/candidate_process/route.ts
+++ gipit-back/app/api/candidate_process/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-  const { candidate_id, process_id, technical_skills, soft_skills, client_comments, match_percent, interview_questions } = await req.json();
+  const { candidate_id, process_id, technical_skills, soft_skills, client_comments, match_percent, interview_questions, total_experience } = await req.json();
 
   try {
     const candidateProcess = await prisma.candidate_process.create({
@@ -15,7 +15,8 @@ export async function POST(req: NextRequest) {
         soft_skills,
         client_comments,
         match_percent,
-        interview_questions
+        interview_questions,
+        total_experience
       },
     });
 
@@ -23,5 +24,21 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     return NextResponse.json({ error: `Error al postear candidate_process: ${error}` }, { status: 500 });
   }
+  
 }
 
+
+
+
+// Manejo del método GET para obtener todos los registros
+export async function GET() {
+  try {
+    const candidateProcesses = await prisma.candidate_process.findMany();
+    return NextResponse.json(candidateProcesses, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Error al obtener candidate_process: ${error}` },
+      { status: 500 }
+    );
+  }
+}
